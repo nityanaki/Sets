5740c0b6ad120d65954725292c2e9b00c60caa0f
Nitya <nityanaki@gmail.com>
2018-11-05 02:59:36 -0600

    Add files via upload

---
+++ Lab07b.java
@@ -0,0 +1,25 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import static java.lang.System.*;
+
+public class Lab07b
+{
+	public static void main( String args[] ) throws IOException
+	{
+		Scanner file = new Scanner(new File("lab07b.dat"));
+		while(file.hasNext())
+		{
+			String sent = file.nextLine();
+			OddEvenSets test = new OddEvenSets(sent);
+			out.println(test);
+		}
+
+	}
+}
---
+++ OddEvenSets.java
@@ -0,0 +1,43 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.Arrays;
+import java.util.Scanner;
+import static java.lang.System.*;
+import java.util.*;
+
+public class OddEvenSets
+{
+	private Set<Integer> odds;
+	private Set<Integer> evens;
+
+	public OddEvenSets()
+	{
+		odds = new TreeSet<Integer>();
+		evens = new TreeSet<Integer>();
+	}
+
+	public OddEvenSets(String line)
+	{
+		odds = new TreeSet<Integer>();
+		evens= new TreeSet<Integer>();
+		String[] hey = line.split(" ");
+		for (int i=0;i<hey.length;i++)
+		{
+			if (Integer.parseInt(hey[i])%2==0)
+				evens.add(Integer.parseInt(hey[i]));
+			if (Integer.parseInt(hey[i])%2==1)
+				odds.add(Integer.parseInt(hey[i]));
+		}
+	}
+
+	public String toString()
+	{
+		return "ODDS : " + odds + "\nEVENS : " + evens + "\n\n";
+	}
+}
